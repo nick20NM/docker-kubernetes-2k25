1f7865d7fdebe3ced3eac33c290c6deb55b71636
Nick <masihnishit@gmail.com>
2025-04-12 16:35:23 +0530

    new project

---
+++ src/test/java/com/alpha/www/docker_kubernetes/DockerKubernetesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alpha.www.docker_kubernetes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerKubernetesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
